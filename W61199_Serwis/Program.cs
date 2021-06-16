df45bbbfb81f1602f75a0649e70cd1185019c03d
Piotr <piotr.kida@hotmail.com>
2021-06-16 16:44:12 +0200

    Add project files.

---
+++ W61199_Serwis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using W61199_Serwis.Views;
+
+namespace W61199_Serwis
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new LoginDialog());
+		}
+	}
+}
---
+++ W61199_Serwis/ServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using W61199_Serwis.Models;
+
+namespace W61199_Serwis
+{
+	class ServiceContext : DbContext
+	{
+		public ServiceContext() : base("ServiceContext") {
+
+			this.Configuration.LazyLoadingEnabled = true;
+		}
+
+		public DbSet<Users> Users { get; set; }
+		public DbSet<Customers> Customers { get; set; }
+		public DbSet<Devices> Devices { get; set; }
+		public DbSet<Repairs> Repairs { get; set; }
+
+	}
+}
